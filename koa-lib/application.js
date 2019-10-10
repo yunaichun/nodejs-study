c279d6e897083c6ec500bc70d20e5abb52efec76
yunaichun <yunaichun@qingting.fm>
2019-10-10 18:02:24 +0800

    koa-convert源码分析

--- koa-lib/application.js
+++ koa-lib/application.js
@@ -5,28 +5,71 @@
  * Module dependencies.
  */
 
+/*判断当前传入的 function 是否是标准的generator function
+  主要是正则判断：var isFnRegex = /^\s*(?:function)?\*/ 
+  /*
+*/
 const isGeneratorFunction = require('is-generator-function');
+/*js 调试工具*/
 const debug = require('debug')('koa:application');
+/*事件监听，当 http 请求关闭，完成或者出错的时候调用注册好的回调*/
 const onFinished = require('on-finished');
+/*响应请求*/
 const response = require('./response');
+/*中间件的函数数组*/
 const compose = require('koa-compose');
+/*判断是否为 json 数据*/
 const isJSON = require('koa-is-json');
+/*运行服务上下文*/
 const context = require('./context');
+/*客户端的请求*/
 const request = require('./request');
+/*请求状态码*/
 const statuses = require('statuses');
+/*事件循环*/
 const Emitter = require('events');
 const util = require('util');
+/*流处理*/
 const Stream = require('stream');
+/*http 请求*/
 const http = require('http');
+/*白名单选择*/
 const only = require('only');
+
+/*兼容旧版本 koa 中间件(利用 co 库)
+  function convert (mw) {
+    // 参数必须是函数
+    if (typeof mw !== 'function') {
+      throw new TypeError('middleware must be a function')
+    }
+    // 参数不是 Generator 函数，直接返回
+    if (mw.constructor.name !== 'GeneratorFunction') {
+      // assume it's Promise-based middleware
+      return mw
+    }
+    // 参数是 Generator 函数
+    const converted = function (ctx, next) {
+      return co.call(ctx, mw.call(ctx, createGenerator(next)))
+    }
+    converted._name = mw._name || mw.name
+    return converted
+  }
+
+  function * createGenerator (next) {
+    return yield next()
+  }
+
+  // co 库的原理是传入 Generator 函数，可以自动执行，返回 Promise 函数
+*/
 const convert = require('koa-convert');
+/*判断当前在运行 koa 的某些接口或者方法是否过期，如果过期，会给出一个升级的提示*/
 const deprecate = require('depd')('koa');
 
 /**
  * Expose `Application` class.
  * Inherits from `Emitter.prototype`.
  */
-
+/*继承了 Emitter : 这个类可以直接为自定义事件注册回调函数和触发事件，同时可以捕捉到其他地方触发的事件*/
 module.exports = class Application extends Emitter {
   /**
    * Initialize a new `Application`.
@@ -47,13 +90,20 @@ module.exports = class Application extends Emitter {
   constructor(options) {
     super();
     options = options || {};
+    /*是否信任 proxy header 参数，默认为 false*/
     this.proxy = options.proxy || false;
+    /*子域默认偏移量，默认为 2*/
     this.subdomainOffset = options.subdomainOffset || 2;
+    /*环境参数，默认为 NODE_ENV 或 'development'*/
     this.env = options.env || process.env.NODE_ENV || 'development';
     if (options.keys) this.keys = options.keys;
+    /*保存通过 app.use(middleware) 注册的中间件*/
     this.middleware = [];
+    /*context 模块，通过 context.js 创建*/
     this.context = Object.create(context);
+    /*request 模块，通过 request.js 创建*/
     this.request = Object.create(request);
+    /*response 模块，通过 response.js 创建*/
     this.response = Object.create(response);
     if (util.inspect.custom) {
       this[util.inspect.custom] = this.inspect;
@@ -69,7 +119,7 @@ module.exports = class Application extends Emitter {
    * @return {Server}
    * @api public
    */
-
+  /*createServer 创建服务*/
   listen(...args) {
     debug('listen');
     const server = http.createServer(this.callback());
@@ -112,13 +162,52 @@ module.exports = class Application extends Emitter {
    * @return {Application} self
    * @api public
    */
-
+  /*添加中间件, 中间件调用方法: 
+    // koa2
+    app.use(async function(ctx, next) { 
+      ctx.test = '123';
+      next();
+    });
+    // koa1
+    app.use(function* (ctx, next) { 
+      ctx.test = '123';
+      yield next;
+    });
+  */
   use(fn) {
+    /*use 必须使用函数*/
     if (typeof fn !== 'function') throw new TypeError('middleware must be a function!');
+    /*如果 fn 是 Generator 函数，说明当前是 koa1 框架，将其转换为 koa2 函数*/
     if (isGeneratorFunction(fn)) {
       deprecate('Support for generators will be removed in v3. ' +
                 'See the documentation for examples of how to convert old middleware ' +
                 'https://github.com/koajs/koa/blob/master/docs/migration.md');
+      /*兼容旧版本 koa 中间件(利用 co 库)
+        function convert (mw) {
+          // 参数必须是函数
+          if (typeof mw !== 'function') {
+            throw new TypeError('middleware must be a function')
+          }
+          // 参数不是 Generator 函数，直接返回
+          if (mw.constructor.name !== 'GeneratorFunction') {
+            // assume it's Promise-based middleware
+            return mw
+          }
+          // 参数是 Generator 函数
+          const converted = function (,ctx next) {
+            return co.call(ctx, mw.call(ctx, createGenerator(next)))
+          }
+          converted._name = mw._name || mw.name
+          return converted
+        }
+
+        function * createGenerator (next) {
+          return yield next()
+        }
+
+        // co 库的原理是传入 Generator 函数，可以自动执行，返回 Promise 函数
+      */
+      // 此 Generator 经过 co 库包装了一层
       fn = convert(fn);
     }
     debug('use %s', fn._name || fn.name || '-');
